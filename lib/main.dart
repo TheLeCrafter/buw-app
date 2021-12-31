c95b9cc8460e0e32d4b396097ae5adbe105a3891
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-12-31 17:53:15 +0100

    + Fixed a possible app crash when using landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:bundesumweltwettbewerbapp/screens/homescreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
