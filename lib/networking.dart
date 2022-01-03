3d8995b0c79bf5aaa1c09d5afa7cec97b9e710d5
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2022-01-03 18:06:34 +0100

    + Update networking.dart to use GitHub instead of the old api

--- lib/networking.dart
+++ lib/networking.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
 
 Future<http.Response> fetchData() async {
-  return await http.get(Uri.https("buw-api.thelecrafter.dev", "data"));
+  return await http.get(Uri.https("raw.githubusercontent.com", "TheLeCrafter/buw-app-data/main/data/app_posts.json"));
 }
